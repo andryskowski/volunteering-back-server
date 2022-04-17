49bafa6dbe6df4f5158d263be537f72bada2dc98
stanislaw.andryskowski <s.andryskowski@gmail.com>
2022-04-17 19:51:22 +0200

    added fixes

--- index.js
+++ index.js
@@ -11,60 +11,9 @@ const messagesRoute = require('./routes/messages');
 const conversationsRoute = require('./routes/conversations');
 const bodyParser = require('body-parser');
 
-/* socket.io */
+const port = process.env.PORT || 8900;
 
-const io = require("socket.io")(8900, {
-    cors: {
-      origin: "https://volunteering-map.herokuapp.com/",
-      origin: "http://localhost:3000/",
-    },
-  });
-  
-  let users = [];
-  
-  const addUser = (userId, socketId) => {
-    !users.some((user) => user.userId === userId) &&
-      users.push({ userId, socketId });
-  };
-  
-  const removeUser = (socketId) => {
-    users = users.filter((user) => user.socketId !== socketId);
-  };
-  
-  const getUser = (userId) => {
-    return users.find((user) => user.userId === userId);
-  };
-  
-  io.on("connection", (socket) => {
-    //when ceonnect
-    console.log("a user connected.");
-  
-    //take userId and socketId from user
-    socket.on("addUser", (userId) => {
-      addUser(userId, socket.id);
-      io.emit("getUsers", users);
-    });
-  
-    //send and get message
-    socket.on("sendMessage", ({ senderId, receiverId, text, date }) => {
-      const user = getUser(receiverId);
-      io.to(user.socketId).emit("getMessage", {
-            senderId,
-            text,
-            date,
-      });
-      console.log(senderId, text, user.socketId, user);
-    });
-  
-    //when disconnect
-    socket.on("disconnect", () => {
-      console.log("a user disconnected!");
-      removeUser(socket.id);
-      io.emit("getUsers", users);
-    });
-  });
-
-/////////
+////////////////
 
 var cors = require('cors');
 app.use(bodyParser.json());
@@ -91,5 +40,54 @@ app.use(express.json());
 //Route Middlewares
 app.use('/api/user', authRoute);
 
-const port = process.env.PORT || 8000;
 app.listen(port, () => console.log('Server Up and running'));
+
+/* socket.io */
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //when ceonnect
+  console.log("a user connected.");
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text, date }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+          senderId,
+          text,
+          date,
+    });
+    console.log(senderId, text, user.socketId, user);
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
+
+/////////
